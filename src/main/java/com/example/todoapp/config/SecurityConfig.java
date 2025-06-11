a212c80624ff65617f85cbb3bd535c6e6bd6cfe6
shunsuke5 <shunsuke1359@gmail.com>
2025-06-11 17:23:27 +0900

    とりあえず認証を無効化

---
+++ src/main/java/com/example/todoapp/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.todoapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .formLogin(fl -> fl.disable());
+
+        return http.build();
+    }
+}
