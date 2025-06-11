b9946faeb0555ab5c3daa9018833e2bde7e33373
shunsuke5 <shunsuke1359@gmail.com>
2025-06-11 17:53:41 +0900

    全Todo取得api 実装完了

---
+++ src/main/java/com/example/todoapp/entity/Todo.java
@@ -0,0 +1,22 @@
+package com.example.todoapp.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "todos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String content;
+}
